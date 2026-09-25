e215ed39bf24dd5482dfba5154214c66feb946df
hans-07 <hansparrahuichaqueo@outlook.com>
2026-09-25 10:44:05 -0300

    delete: eliminar archivos SQL de inserción de direcciones, proveedores y empresas

--- datos/sql/2026.09.14_10.57.insert_propvedores.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO proveedores (nombre_proovedor, telefono, correo, empresa) VALUES
-('Distribuidora de Carnes El Novillo Alegre', '+56228889911', 'ventas@elnovilloalegre.cl', 1),
-('Comercializadora de Frutas La Vega SpA', '+56911223344', 'pedidos@lavegadechile.cl', 1),
-('Suministros Gastronómicos Central', '+56227776655', 'contacto@centralabarrotes.cl', 1);
--- datos/sql/2026.09.14_10.59_insert_empresa.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO empresas (nombre_empresa, web, correo, telefono, direccion) VALUES
-('Antojos del Sur SpA', 'www.antojosdelsur.cl', 'contacto@antojosdelsur.cl', '+56977665544', 1);
